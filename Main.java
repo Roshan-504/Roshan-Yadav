d1f7c49224303c0eb071a6a6afed100f56c6442c
Roshan <Roshan@gmail.com>
2024-08-14 01:07:01 +0530

    iRevert "Update Main.java with modify method"
    This reverts commit 5e55fb6465da9cea86392ccda5dd1f42321f965e.

--- Main.java
+++ Main.java
@@ -1,10 +1,1 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        modify();
-    }
-
-    public static void modify() {
-        System.out.println("modified method");
-    }
-}
+public class Main { public static void main(String[] args) { System.out.println("Hello, World!"); } }
