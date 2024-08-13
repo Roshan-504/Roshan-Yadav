5e55fb6465da9cea86392ccda5dd1f42321f965e
Roshan <Roshan@gmail.com>
2024-08-14 01:05:46 +0530

    Update Main.java with modify method

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
-public class Main { public static void main(String[] args) { System.out.println("Hello, World!"); } }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        modify();
+    }
+
+    public static void modify() {
+        System.out.println("modified method");
+    }
+}
